f4828882a5b42b821b086d1a6eedbdce47841c28
Arun S <arunlegilsspba@gmail.com>
2025-08-23 07:22:47 -0400

    Update vite.config.js

--- lms/frontend/vite.config.js
+++ lms/frontend/vite.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@Landingpage': path.resolve(__dirname, 'src/Landingpage'),
+      '@About': path.resolve(__dirname, 'src/About'),
+      '@Admin': path.resolve(__dirname, 'src/Admin'),
+      '@Blog': path.resolve(__dirname, 'src/Blog'),
+      '@Book': path.resolve(__dirname, 'src/Book'),
+      '@Checkout': path.resolve(__dirname, 'src/Checkout'),
+      '@Contact': path.resolve(__dirname, 'src/Contact'),
+      '@User': path.resolve(__dirname, 'src/Userprofile'),
+      '@Lms': path.resolve(__dirname, 'src/Lms'),
+      '@Scroll': path.resolve(__dirname, 'src/ScrollToTop.jsx'),
+      '@Protected': path.resolve(__dirname, 'src/ProtectedRoute.jsx'),
+      '@Forgot': path.resolve(__dirname, 'src/ForgotPassword')
+    }
+  },
   server: {
     host: true,
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'http://backend:5000' // Docker container name mapping
     }
   }
-})
+});
