c51b1ca7e05049ccdf97c098824095e058882ca2
Arun S <arunlegilsspba@gmail.com>
2025-08-25 03:21:11 -0400

    Update scan-imports.js

--- lms/scan-imports.js
+++ lms/scan-imports.js
@@ -1,28 +1,29 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-// Absolute path to the src folder
-const srcDir = path.join(__dirname, "../frontend/src"); // Adjust based on your Dockerfile context
+// Set the correct frontend src directory
+const srcDir = path.join(__dirname, 'frontend/src'); // __dirname points to lms folder
 
 function getAllFiles(dirPath, arrayOfFiles = []) {
   if (!fs.existsSync(dirPath)) {
-    console.error("Directory does not exist:", dirPath);
+    console.error('Directory does not exist:', dirPath);
     process.exit(1);
   }
 
   const files = fs.readdirSync(dirPath);
 
-  files.forEach(function(file) {
-    const filePath = path.join(dirPath, file);
-    if (fs.statSync(filePath).isDirectory()) {
-      arrayOfFiles = getAllFiles(filePath, arrayOfFiles);
+  files.forEach(function (file) {
+    const fullPath = path.join(dirPath, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      arrayOfFiles = getAllFiles(fullPath, arrayOfFiles);
     } else {
-      arrayOfFiles.push(filePath);
+      arrayOfFiles.push(fullPath);
     }
   });
 
   return arrayOfFiles;
 }
 
+// Run the script
 const allFiles = getAllFiles(srcDir);
-console.log("All files found:", allFiles.length);
+console.log('Found files:', allFiles.length);
